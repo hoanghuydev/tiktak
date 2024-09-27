82e5f3b75153169ccd45d69313e224df7502fd98
hoanghuydev <tranvohoanghuy12ab@gmail.com>
2024-09-27 16:53:38 +0700

    fix : fix padding of post comment

--- client/src/site/Post.tsx
+++ client/src/site/Post.tsx
@@ -13,12 +13,15 @@ import { AppDispatch } from '@/redux/store';
 import { setPost } from '@/features/post/postSlice';
 import PostComment from './components/Post/PostComment';
 import CommentForm from './components/Post/CommentForm';
+import clsx from 'clsx';
 
 const Post = () => {
   let { postId } = useParams();
-  const parsedPostId = parseInt(postId ?? '0');
+  const parsedPostId = parseInt(postId ?? '0', 10);
   const post = useSelector(getPostSelector);
   const postSlice = useSelector(postSliceSelector);
+  const commentFormRef = useRef<HTMLDivElement | null>(null);
+  const [paddingBottom, setPaddingBottom] = useState<number>(0);
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
@@ -29,9 +32,27 @@ const Post = () => {
     };
     getPostInfoById(parsedPostId);
   }, [parsedPostId]);
+  // Update paddingBottom when CommentForm change height
+  useEffect(() => {
+    const commentFormElement = commentFormRef.current;
+
+    if (!commentFormElement) return;
+
+    const resizeObserver = new ResizeObserver(() => {
+      setPaddingBottom(commentFormElement.clientHeight);
+    });
+
+    resizeObserver.observe(commentFormElement);
+
+    return () => {
+      if (commentFormElement) {
+        resizeObserver.unobserve(commentFormElement);
+      }
+    };
+  }, []);
 
   return (
-    <div className=" h-full">
+    <div className="h-full">
       {!postSlice?.isLoading && post && (
         <div className="flex h-full">
           {/* First row: video */}
@@ -45,12 +66,18 @@ const Post = () => {
           </div>
 
           {/* Second row: post details */}
-          <div className="flex-[100%] h-full overflow-hidden relative bg-white  flex-col min-w-[290px]">
-            <div className=" overflow-y-auto">
+          <div className="flex-[100%] h-full overflow-hidden relative bg-white flex-col min-w-[290px]">
+            <div
+              className={clsx('overflow-y-auto h-full')}
+              style={{
+                paddingBottom: `${paddingBottom}px`,
+              }}
+            >
               <PostInfo />
               <PostComment />
             </div>
             <CommentForm
+              ref={commentFormRef}
               post={post}
               className="min-h-[85px] max-h-[166px] absolute left-0 right-0 bottom-0"
             />
