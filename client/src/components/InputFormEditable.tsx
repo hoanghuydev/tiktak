53f716832e99b8d1035c6d740ada00c3b4afc149
hoanghuydev <tranvohoanghuy12ab@gmail.com>
2024-10-31 15:49:32 +0700

    fix : input content editable

--- client/src/components/InputFormEditable.tsx
+++ client/src/components/InputFormEditable.tsx
@@ -18,12 +18,12 @@ import React, { forwardRef, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-interface CommentFormProps {
+interface InputFormEditableProps {
   parentCommentId?: number;
   className?: string;
 }
 
-const CommentForm = forwardRef<HTMLDivElement, CommentFormProps>(
+const InputFormEditable = forwardRef<HTMLDivElement, InputFormEditableProps>(
   ({ className, parentCommentId }, ref) => {
     const { isSuccess } = useSelector((state: RootState) => state.comment);
     const post = useSelector(getPostSelector);
@@ -110,7 +110,7 @@ const CommentForm = forwardRef<HTMLDivElement, CommentFormProps>(
                     onFocus={handleFocus}
                     onBlur={handleBlur}
                     suppressContentEditableWarning={true}
-                    className="py-3 border-none min-w-[180px] flex-1 whitespace-pre-wrap select-text break-words break-all text-wrap outline-none h-fit max-h-[80px] overflow-y-auto bg-transparent text-[14px] text-[#161823e2]"
+                    className="my-3 border-none min-w-[180px] flex-1 whitespace-pre-wrap select-text break-words break-all text-wrap outline-none h-fit max-h-[80px] overflow-y-auto bg-transparent text-[14px] text-[#161823e2]"
                   >
                     {isFocused || commentTextRef.current
                       ? commentTextRef.current
@@ -118,6 +118,7 @@ const CommentForm = forwardRef<HTMLDivElement, CommentFormProps>(
                   </p>
                   <EmojiPicker
                     inputRef={commentInputRef}
+                    commentText={commentTextRef.current}
                     setCommentText={(text: string) => {
                       commentTextRef.current = text; // Update the text with the emoji inserted
                     }}
@@ -155,4 +156,4 @@ const CommentForm = forwardRef<HTMLDivElement, CommentFormProps>(
   }
 );
 
-export default CommentForm;
+export default InputFormEditable;
